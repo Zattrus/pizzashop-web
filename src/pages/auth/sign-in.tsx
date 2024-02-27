3d52509fbf01daeedba2a2954a840f919b148b22
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2024-02-27 14:33:44 -0300

    Adiciona dependência do pacote "sonner" e implementa notificações de sucesso e erro na autenticação

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,11 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-
 const signInForm = z.object({
   email: z.string().email(),
 })
@@ -22,7 +22,18 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     console.log(data)
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas!')
+    }
   }
 
   return (
